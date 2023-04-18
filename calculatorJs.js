72d7dcadb001b142263b856df17be43cc43b8a6f
ATANU MONDAL <mondalatanu756@gmail.com>
2023-04-18 12:39:55 +0530

    first commit

---
+++ calculatorJs.js
@@ -0,0 +1,20 @@
+let Str = "";
+let buttons = document.querySelectorAll('.btn');
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            Str = eval(Str);
+            document.querySelector('input').value = Str;
+        } else if (e.target.innerHTML == 'AC') {
+            Str = "";
+            document.querySelector('input').value = Str;
+        } else if (e.target.innerHTML == 'DEL') {
+            Str = Str.substring(0, Str.length - 1);
+            document.querySelector('input').value = Str;
+        } else {
+            console.log(e.target)
+            Str = Str + e.target.innerHTML;
+            document.querySelector('input').value = Str;
+        }
+    })
+})
